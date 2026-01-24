23b956ad3172759f4f7ea4a180f6afe04565e585
Michael Kadzioch <michael@kadzioch.net>
2026-01-24 11:32:29 +0100

    finish Gui Mwst calculator

--- gui-mwstrechner.py
+++ gui-mwstrechner.py
@@ -6,16 +6,35 @@ def btnClose_click():
     if messagebox.askokcancel('Programm Beenden', 'Soll das Programm wirklich beendet werden?'):
         appWindow.destroy()
 
+
+
 # def calculateMwst(value, mwst_value):
 def calculateMwst():
     # global ebValue, cbMwstVar
-    print('Mehrwertsteuer berechnen...', ebValue.get(), cbMwstVar.get())
+    
+    # Mwst-String formatieren und umwandeln
+    mwstStr = cbMwstVar.get()
+    mwstFloat = float(mwstStr.replace("%", "").strip())
+
+    # Usereingabe prüfen und in Float umwandeln
+    try:
+        value = float(ebValue.get())
+    except ValueError:
+        messagebox.showerror('Fehler', 'Bitte eine gültige Zahl eingeben.')
+        return
+    else:
+        # print('Mehrwertsteuer berechnen...', value * (1 + (mwstFloat / 100)))
+        # Mehrwertsteuer berechnen und ausgeben
+        labelOutput.config(text=f'Ergebnis: {value * (1 + (mwstFloat / 100)):.2f}')
+        return 
+
+
 
 def main():
     global appWindow
     appWindow = tkinter.Tk()
     appWindow.title('Mehrwertsteuer Berechnen')
-    appWindow.geometry('500x400')
+    appWindow.geometry('500x200')
 
     appWindow.columnconfigure(0, weight=1)
     appWindow.columnconfigure(1, weight=1)
@@ -39,8 +58,12 @@ def main():
     cbMwst = tkinter.OptionMenu(appWindow, cbMwstVar, '0 %', '7 %', '19 %')
     cbMwst.grid(row=0, column=1, padx=15, pady=15, sticky='w')
 
+    global labelOutput
+    labelOutput = tkinter.Label(appWindow, text='', font=('', 18))
+    labelOutput.grid(row=1, column=0, padx=15, pady=15, sticky='w')
+
     btnCalculate = tkinter.Button(appWindow, text='Berechnen', command=calculateMwst)
-    btnCalculate.grid(row=1, column=0, padx=15, pady=15, sticky='e')
+    btnCalculate.grid(row=1, column=1, padx=15, pady=15, sticky='w')
 
     appWindow.mainloop()
 
